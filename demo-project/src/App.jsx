5799f2900649273c8178b57be15fb936569b4e60
Omkar Raykar <raykaromkar1244@gmail.com>
2024-09-03 17:36:36 +0530

    done with blog app and finished reactJs

--- demo-project/src/App.jsx
+++ demo-project/src/App.jsx
@@ -1,10 +1,39 @@
-function App() {
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authServive from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from "./component/Header/Header";
+import Footer from "./component/Footer/Footer";
+import { Outlet } from "react-router-dom";
 
-  return (
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authServive
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  return !loading ? (
     <>
-    <h1>works</h1>
+      <div className="min-h-sc flex flex-wrap content-between bg-gray-400">
+        <div className="w-full block">
+          <Header />
+          <main>
+            <Outlet />
+          </main>
+          <Footer />
+        </div>
+      </div>
     </>
-  )
+  ) : null;
 }
 
-export default App
+export default App;
